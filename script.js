44fa76528f51f09eabdf82230e9f144251135aa7
lipsa das <lipsalipsadas37@gmail.com>
2026-06-22 19:30:15 +0530

    pujacoonect project

---
+++ script.js
@@ -0,0 +1,21 @@
+function bookPuja() {
+    alert("Puja booked successfully!");
+}
+function bookPuja() {
+
+    const puja = document.querySelector("select").value;
+    const date = document.querySelector('input[type="date"]').value;
+
+    localStorage.setItem("puja", puja);
+    localStorage.setItem("date", date);
+
+    alert("Booking Saved!");
+}
+fetch("http://localhost:5000/user")
+.then(res => res.json())
+.then(data => {
+    console.log(data);
+
+    document.getElementById("name").value = data[0].fullname;
+    document.getElementById("phone").value = data[0].mobile;
+});
---
+++ server.js
@@ -0,0 +1,134 @@
+const express = require("express");
+const db = require("./db");
+const cors = require("cors");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("PujaConnect Backend Running");
+});
+app.use(cors());
+app.use(express.static("public"));
+app.use(express.json());
+
+app.post("/register", (req, res) => {
+    const { name, email, password } = req.body;
+
+    const sql =
+        "INSERT INTO users(name, email, password) VALUES (?, ?, ?)";
+
+    db.query(sql, [name, email, password], (err, result) => {
+        if (err) {
+            console.log(err);
+            return res.send("Registration Failed");
+        }
+
+        res.send("User Registered Successfully");
+    });
+});
+
+    app.get("/user", (req, res) => {
+    db.query("SELECT * FROM bookings", (err, result) => {
+        if (err) {
+            return res.status(500).json(err);
+        }
+        res.json(result);
+    });
+});
+
+app.post("/login", (req, res) => {
+    const { email, password } = req.body;
+
+    const sql = "SELECT * FROM users WHERE email=? AND password=?";
+
+    db.query(sql, [email, password], (err, result) => {
+        if (err) {
+            return res.send("Error");
+        }
+
+        if (result.length > 0) {
+            res.send("Login successful");
+        } else {
+            res.send("Invalid credentials");
+        }
+    });
+});
+
+app.get("/",(req, res) => {
+    res.send("server is working");
+});
+
+app.post("/book-puja", (req, res) => {
+
+    
+    const { name, phone, puja_type, booking_date, booking_time } = req.body;
+
+const sql = `
+INSERT INTO bookings
+(name, phone, puja_type, booking_date, booking_time, status)
+VALUES (?, ?, ?, ?, ?, ?)
+`;
+
+db.query(
+    sql,
+    [name, phone, puja_type, booking_date, booking_time, "pending"],
+    (err, result) => {
+        if (err) {
+            console.log(err);
+            return res.send("Booking Failed");
+        }
+
+        res.send("Booking Successful");
+    });
+});    
+app.post("/update-status", (req, res) => {
+    const { id, status } = req.body;
+
+    const sql = "UPDATE bookings SET status=? WHERE id=?";
+
+    db.query(sql, [status, id], (err) => {
+        if (err) {
+            console.log(err);
+            return res.send("Error");
+        }
+        res.send("Status Updated");
+    });
+});
+app.put("/approve-booking/:id", (req, res) => {
+
+    const bookingId = req.params.id;
+
+    const sql =
+    "UPDATE bookings SET status='Approved' WHERE id=?";
+
+    db.query(sql, [bookingId], (err, result) => {
+
+        if (err) {
+            console.log(err);
+            return res.send("Error");
+        }
+
+        res.send("Booking Approved");
+    });
+});
+app.put("/reject-booking/:id", (req, res) => {
+
+    const bookingId = req.params.id;
+
+    const sql =
+    "UPDATE bookings SET status='Rejected' WHERE id=?";
+
+    db.query(sql, [bookingId], (err, result) => {
+
+        if (err) {
+            console.log(err);
+            return res.send("Error");
+        }
+
+        res.send("Booking Rejected");
+    });
+});
+
+app.listen(5000, () => {
+    console.log("Server Running on Port 5000");
+});
